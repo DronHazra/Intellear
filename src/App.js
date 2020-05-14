39aa42b32d025e6dde7d3f8b2bee035b43890704
Dron <dron@intellear.com>
2020-05-14 12:05:27 -0400

    Take 2

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import TemperatureSlider from './components/slider'
 import GenerateButton from './components/generate_button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import { MusicVAE } from '@magenta/music/node/music_vae';
+import PlayButton from './components/play_button';
 
-const magentaModel = require('@magenta/music/node/music_vae');
 
 export default function App() {
   const [value, setVal] = useState(1.0)
   const url = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_4bar_med_lokl_q2';
   const[modelDisabled, disableModel] = useState(true);
+
   const loadModel = () => {
-    const model = new magentaModel.MusicVAE(url);
+    const model = new MusicVAE(url);
     model.initialize().then(() => {
         console.log(modelDisabled);
         disableModel(false);
@@ -19,12 +21,12 @@ export default function App() {
     });
     return model
   }
+
   const [musicvae, init] = useState(loadModel);
 
   const tempCallback = (val) => {
     setVal(val);
   }
-  const [player, setPlayer] = useState(new core.SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus'));
 
   const generate = () => {
     disableModel(true);
@@ -34,6 +36,7 @@ export default function App() {
       newSample(sample[0]);
     });
   }
+
   const [currentSample, newSample] = useState('');
 
   return (
@@ -46,6 +49,9 @@ export default function App() {
         </Grid>
         <Grid item>
           <GenerateButton temperature={value} callback={generate} disabled={modelDisabled} />
+        </Grid>
+        <Grid item>
+          <PlayButton sequence={currentSample} />
         </Grid>
         <Grid item xs={5} />
       </Grid>
--- src/components/play_button.js
+++ src/components/play_button.js
@@ -1,7 +1,18 @@
-import React from "react";
-import IconButton, { Icon } from '@material-ui/core';
-import SoundFontPlayer from '@magenta/music/node/core/SoundFontPlayer'
+import React, { useState } from "react";
+import IconButton from '@material-ui/core/IconButton';
+import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+import { SoundFontPlayer } from '@magenta/music/node/core';
 
 export default function PlayButton(props) {
-    
+    const [player, setPlayer] = useState(new SoundFontPlayer('https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus'));
+    //player.playClick = true;
+
+    const play = () => {
+        player.start(props.sequence).then(() => player.stop());
+    }
+    return(
+        <IconButton aria-label="play" color="secondary" onClick={play} >
+            <PlayCircleFilledIcon />
+        </IconButton>
+    )
 }
