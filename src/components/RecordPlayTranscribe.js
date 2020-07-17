ced820b2553332c65d5524b45a0ff43965f05d01
Dron <dronh.to@gmail.com>
2020-07-17 19:39:02 -0400

    added a step in the process

--- src/components/RecordPlayTranscribe.js
+++ src/components/RecordPlayTranscribe.js
@@ -6,13 +6,14 @@ import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import StopIcon from '@material-ui/icons/Stop';
 import Typography from '@material-ui/core/Typography';
 import PlayButtton from './play_button';
-import { StepContext } from '../App';
+import { AppContext } from '../App';
 import PlayRecordingButton from './play_recording_button';
 import { ReactMic } from 'react-mic';
 import { makeStyles } from '@material-ui/styles';
 import TranscribeButton from './transcribe_button';
-import { Fade } from '@material-ui/core';
+import { Fade, Snackbar } from '@material-ui/core';
 import notes from '../notes';
+import { Alert } from '@material-ui/lab';
 
 const useStyles = makeStyles({
 	root: {
@@ -24,16 +25,21 @@ export default function RecordPlayTranscribe(props) {
 	const classes = useStyles();
 	//const [permission, setPermission] = useState(false);
 	const [audioURL, setAudioURL] = useState('');
+	const [open, setOpen] = useState(false);
 	const [recording, setRecord] = useState(false);
-	const step = useContext(StepContext);
+	const step = useContext(AppContext);
 
 	const toggleRecord = () => {
-		if (recording) {
-			console.log(recording);
-			setRecord(false);
-			step.changeStep(3);
+		if (step.recordingBroken) {
+			setOpen(true);
 		} else {
-			setRecord(true);
+			if (recording) {
+				console.log(recording);
+				setRecord(false);
+				step.changeStep(3);
+			} else {
+				setRecord(true);
+			}
 		}
 	};
 	/*useEffect(() => {
@@ -49,75 +55,96 @@ export default function RecordPlayTranscribe(props) {
 		setAudioURL(URL.createObjectURL(recordedBlob.blob));
 	};
 	return (
-		<Grid container direction='row' alignItems='center' justify='center'>
-			<Grid item xs={12}>
-				<Fade
-					in={true}
-					timeout={props.timeout}
-					style={{ transitionDelay: props.delay }}
-				>
-					<ButtonGroup
-						color='secondary'
-						variant='contained'
-						fullWidth
-						disableElevation
+		<>
+			<Grid
+				container
+				direction='row'
+				alignItems='center'
+				justify='center'
+			>
+				<Grid item xs={12}>
+					<Fade
+						in={true}
+						timeout={props.timeout}
+						style={{ transitionDelay: props.delay }}
 					>
-						<PlayButtton
-							sequence={props.sequence}
-							disabled={step.step === 0}
-							tempo={props.tempo}
-						/>
-						<Button
-							startIcon={
-								recording ? (
-									<StopIcon />
-								) : (
-									<FiberManualRecordIcon />
-								)
-							}
-							onClick={toggleRecord}
-							disabled={step.step !== 2}
+						<ButtonGroup
+							color='secondary'
+							variant='contained'
+							fullWidth
+							disableElevation
 						>
-							Record
-						</Button>
-						<PlayRecordingButton
-							url={audioURL}
-							disabled={audioURL ? false : true}
-						/>
-						<TranscribeButton
-							url={audioURL}
-							sequence={props.sequence}
-							sequenceCallback={props.callback}
-						/>
-					</ButtonGroup>
-				</Fade>
-			</Grid>
-			<Grid item>
-				<Fade
-					in={props.sequence ? true : false}
-					timeout={props.timeout}
-				>
-					<Typography align='left'>
-						<strong>First note: </strong>
+							<PlayButtton
+								sequence={props.sequence}
+								disabled={step.step !== 1 && step.step !== 2}
+								tempo={props.tempo}
+							/>
+							<Button
+								startIcon={
+									recording ? (
+										<StopIcon />
+									) : (
+										<FiberManualRecordIcon />
+									)
+								}
+								onClick={toggleRecord}
+								disabled={step.step !== 2}
+							>
+								Record
+							</Button>
+							<PlayRecordingButton
+								url={audioURL}
+								disabled={step.step < 3}
+							/>
+							<TranscribeButton
+								url={audioURL}
+								sequence={props.sequence}
+								sequenceCallback={props.callback}
+							/>
+						</ButtonGroup>
+					</Fade>
+				</Grid>
+				<Grid item>
+					<Fade
+						in={props.sequence ? true : false}
+						timeout={props.timeout}
+					>
+						<Typography align='left'>
+							<strong>First note: </strong>
 
-						{props.sequence
-							? notes[props.sequence.notes[0].pitch]
-							: ''}
-					</Typography>
-				</Fade>
+							{props.sequence
+								? notes[props.sequence.notes[0].pitch]
+								: ''}
+						</Typography>
+					</Fade>
+				</Grid>
+				<Grid item xs='auto'>
+					<ReactMic
+						record={recording}
+						onStop={handleStop}
+						channelCount={1}
+						className={classes.root}
+						strokeColor={'#00e5ff'}
+						backgroundColor={'#fafafa'}
+						echoCancellation
+						noiseSuppression
+					/>
+				</Grid>
 			</Grid>
-			<Grid item xs='auto'>
-				<ReactMic
-					record={recording}
-					onStop={handleStop}
-					channelCount={1}
-					className={classes.root}
-					strokeColor={'#00e5ff'}
-					backgroundColor={'#fafafa'}
-					echoCancellation
-					noiseSuppression
-				/>
-			</Grid>
-		</Grid>
+			<Snackbar
+				anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+				open={open}
+				onClose={() => setOpen(false)}
+			>
+				<Alert
+					variant='filled'
+					onClose={() => setOpen(false)}
+					severity='error'
+				>
+					Uh oh, something went wrong:(. We couldn't get recording to
+					work on your device. Sorry ;-;
+				</Alert>
+			</Snackbar>
+		</>
 	);
 }
--- src/components/play_recording_button.js
+++ src/components/play_recording_button.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Button from '@material-ui/core/Button';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import PauseIcon from '@material-ui/icons/Pause';
+import { AppContext } from '../App';
+import { useContext } from 'react';
 
 export default function PlayRecordingButton(props) {
+	const step = useContext(AppContext);
 	const [playing, toggle] = useState(false);
 	const [player, setPlayer] = useState(new Audio(props.url));
 	const handleClick = () => {
@@ -11,6 +13,7 @@ export default function PlayRecordingButton(props) {
 			player.pause();
 			toggle(false);
 		} else {
+			step.changeStep(4);
 			player.play();
 			toggle(true);
 		}
