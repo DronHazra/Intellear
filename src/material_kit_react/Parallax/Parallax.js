f226ca89ae9fda316143e88d29bf3c2892efe3f6
Dron <dronh.to@gmail.com>
2020-07-17 19:40:30 -0400

    reduced parallax speed

--- src/material_kit_react/Parallax/Parallax.js
+++ src/material_kit_react/Parallax/Parallax.js
@@ -32,7 +32,7 @@ export default function Parallax(props) {
 		};
 	});
 	const resetTransform = () => {
-		var windowScrollTop = window.pageYOffset / 3;
+		var windowScrollTop = window.pageYOffset / 4;
 		setTransform('translate3d(0,' + windowScrollTop + 'px,0)');
 	};
 	const { filter, className, children, style, image, small } = props;
