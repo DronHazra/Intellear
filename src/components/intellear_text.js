c3fc9f5cce7f4b49530a63bf9f75186e813a2a3b
Dron <dronh.to@gmail.com>
2020-07-17 20:40:41 -0400

    changed top text box

--- src/components/intellear_text.js
+++ src/components/intellear_text.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { Card, Fade, CardContent, Grid, Typography } from '@material-ui/core';
+import {
+	Card,
+	Fade,
+	CardContent,
+	Grid,
+	Typography,
+	makeStyles,
+} from '@material-ui/core';
 
+const useStyles = makeStyles((theme) => {
+	return {
+		root: {
+			background: theme.palette.background.default,
+		},
+	};
+});
 export default function TextCard(props) {
 	let { score, ...fadeProps } = props;
+	const classes = useStyles();
 	return (
 		<Fade {...fadeProps}>
-			<Card elevation={3}>
+			<Card elevation={3} className={classes.root}>
 				<CardContent>
 					<Grid container direction='column' alignItems='center'>
 						<Grid item>
@@ -15,14 +30,11 @@ export default function TextCard(props) {
 					<Typography align='justify'>
 						Welcome to Intellear! To start, click{' '}
 						<strong>Generate</strong> and <strong>Listen</strong> to
-						the AI-Generated sample! Once you're ready,{' '}
-						<strong>Record</strong> yourself playing the sample
-						back, and click <strong>Score</strong> to get graded!
-					</Typography>
-					<Typography align='justify'>
-						NOTE: Some AI samples might be a little difficult. You
-						can try slowing down the tempo, or reducing the
-						temperature and re-generating.
+						the AI-Generated melody! Once you're ready,{' '}
+						<strong>Record</strong> yourself playing it back, and
+						click <strong>Score</strong> to get graded! Your robots
+						will be working hard, so your browser might become
+						sluggish. Good luck!
 					</Typography>
 				</CardContent>
 			</Card>
