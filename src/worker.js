3bc735e8c62f12a8c4cf07edf73c5cb989fbc63f
Dron <dronh.to@gmail.com>
2020-09-03 09:24:55 -0400

    Revert "Revert "web worker working!""
    This reverts commit 236fdb61cc574a90da878bbf1e12196404893d7d.

--- src/worker.js
+++ src/worker.js
@@ -1,17 +1,17 @@
 // from magenta docs
 /*eslint-disable*/
-// importScripts(
-// 	'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.4.0/dist/tf.min.js'
-// );
-// importScripts(
-// 	'https://cdn.jsdelivr.net/npm/@magenta/music@^1.12.0/es6/core.js'
-// );
-// importScripts(
-// 	'https://cdn.jsdelivr.net/npm/@magenta/music@^1.12.0/es6/music_vae.js'
-// );
-const model = require('@magenta/music/es6/musicvae.js');
+self.importScripts(
+	'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.4.0/dist/tf.min.js'
+);
+self.importScripts(
+	'https://cdn.jsdelivr.net/npm/@magenta/music@^1.12.0/es6/core.js'
+);
+self.importScripts(
+	'https://cdn.jsdelivr.net/npm/@magenta/music@^1.12.0/es6/music_vae.js'
+);
+// const model = require('@magenta/music/es6/musicvae.js');
 
-const mvae = new model.MusicVAE(
+const mvae = new music_vae.MusicVAE(
 	'https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_4bar_med_lokl_q2'
 );
 
